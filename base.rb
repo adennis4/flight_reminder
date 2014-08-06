82174727e39240a88e3a5ba8136be98c043ade7d
Andrew Dennis <adennis4@gmail.com>
2014-08-06 09:24:20 -0500

    Setup mechanize to scrape SW

--- base.rb
+++ base.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'rack-flash'
+require 'mechanize'
 require 'pry'
 require './environments'
 require 'haml'
@@ -17,6 +18,7 @@ class FlightCheckin < Sinatra::Base
     @record = FlightRecord.new params
 
     if @record.save
+      TravelAgent.new @record
       flash[:notice] = "#{@record.first_name}, you will be automatically checked-in for your flight."
       redirect '/'
     else
@@ -32,5 +34,23 @@ end
 
 class FlightRecord < ActiveRecord::Base
   validates_presence_of :confirmation, :first_name, :last_name, :email, :flight_date, :flight_time, :departure_time_zone
+end
 
+class TravelAgent
+
+  def initialize(record)
+    mechanize_setup
+    @record = record
+  end
+
+  def mechanize_setup
+    agent = Mechanize.new
+
+    page = agent.get "http://www.southwest.com"
+    review_page = page.form_with(id: 'retrieveItinerary') do |form|
+      form.field_with(name: 'confirmationNumber').value = 'ABC123'
+      form.field_with(name: 'firstName').value = 'Lionel'
+      form.field_with(name: 'lastName').value = 'Messi'
+    end.submit
+  end
 end
